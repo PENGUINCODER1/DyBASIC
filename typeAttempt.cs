57a01c1c2f899a9fae008541fe2bc4dff92261d5
CoderPenguin1 <76060071+PENGUINCODER1@users.noreply.github.com>
2023-12-05 20:27:08 -0500

    Create typeAttempt.cs

---
+++ typeAttempt.cs
@@ -0,0 +1,46 @@
+namespace typeAttempt
+{
+  /// <summary>
+  /// Used To Try To Convert To (x) Type
+  /// </summary>
+  public class TypeAttempt
+  {
+    // No STR (string) check as it's feasible to put in main.cs when needed
+    
+    /// <summary>
+    /// Try To Convert To INT
+    /// </summary>
+    /// <returns>true If Successful Conversion Attempt; false If Failed Conversion Attempt
+    ///</returns>
+    public bool Int(dynamic input)
+    {
+      try
+      {
+        int.Parse(input);
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+    }
+
+    /// <summary>
+    /// Try To Convert To FLT
+    /// </summary>
+    /// <returns>true If Successful Conversion Attempt; false If Failed Conversion Attempt
+    ///</returns>
+    public bool Float(dynamic input)
+    {
+      try
+      {
+        float.Parse(input);
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+    }
+  }
+}
