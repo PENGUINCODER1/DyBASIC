76a1cdce260473ace80487ccd0e0954762cbcb73
CoderPenguin1 <76060071+PENGUINCODER1@users.noreply.github.com>
2024-01-07 02:16:11 -0500

    Create vars.cs
    Prep for future refactor.

---
+++ vars.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace Variables
+{
+  // Integers
+  public static class IntVar
+  {
+    public static List<string> intNames = new();
+    public static List<int> intVals = new();
+  }
+
+  // Strings
+  public static class StrVar
+  {
+    public static List<string> strNames = new();
+    public static List<string> strVals = new();
+  }
+
+  // Floats
+  public static class FltVar
+  {
+    public static List<string> fltNames = new();
+    public static List<float> fltVals = new();
+  }
+}
