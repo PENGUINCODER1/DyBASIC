49541aee6890c4dd46833181e9d95374f0b156d9
CoderPenguin1 <76060071+PENGUINCODER1@users.noreply.github.com>
2024-01-18 23:29:29 -0500

    Update vars.cs
    Reformat

--- vars.cs
+++ vars.cs
@@ -2,24 +2,24 @@ using System.Collections.Generic;
 
 namespace Variables
 {
-  // Integers
-  public static class IntVar
-  {
-    public static List<string> intNames = new();
-    public static List<int> intVals = new();
-  }
+    // Integers
+    public static class IntVar
+    {
+        public static List<string> intNames = new();
+        public static List<int> intVals = new();
+    }
 
-  // Strings
-  public static class StrVar
-  {
-    public static List<string> strNames = new();
-    public static List<string> strVals = new();
-  }
+    // Strings
+    public static class StrVar
+    {
+        public static List<string> strNames = new();
+        public static List<string> strVals = new();
+    }
 
-  // Floats
-  public static class FltVar
-  {
-    public static List<string> fltNames = new();
-    public static List<float> fltVals = new();
-  }
+    // Floats
+    public static class FltVar
+    {
+        public static List<string> fltNames = new();
+        public static List<float> fltVals = new();
+    }
 }
